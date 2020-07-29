6338ce0a5e892ab9427b3dfc18d89093bf20eca9
SpPap <spiros_pd@hotmail.com>
2020-07-29 19:06:03 +0300

    added speaker

--- parking_sensor.ino
+++ parking_sensor.ino
@@ -2,8 +2,9 @@
 
 int echoPin = 2;
 int trigPin = 3;
+int speakerPin = 5;
 int redPin = 4;
-int greenPin = 5;
+int greenPin = 1;
 int yellowPins[] = {6, 7};
 int rs = 12;
 int enable = 13;
@@ -28,9 +29,10 @@ void loop()
 
     long duration = 0, distanceCM = 0;
     long FirstWarningDistance = 50;
-    long SecondWarningDistance = 25;
+    long SecondWarningDistance = 30;
+    long ThirdWarningDistance = 20;
     long maxDistance = 10;
-    int dt = 200; //delay time (LCD)
+    int lcd_dt = 250; //delay time (LCD)
 
     //clear trigPin
     digitalWrite(trigPin, LOW);
@@ -48,31 +50,24 @@ void loop()
 
     if (distanceCM <= maxDistance)
     {
-        digitalWrite(greenPin, HIGH);
-        digitalWrite(yellowPins[0], HIGH);
-        digitalWrite(yellowPins[1], HIGH);
-        digitalWrite(redPin, HIGH);
+        finalWarning();
+        }
+    else if (distanceCM <= ThirdWarningDistance)
+    {
+        thirdWarning(distanceCM, ThirdWarningDistance);
+        redBlink(distanceCM, ThirdWarningDistance);
     }
     else if (distanceCM <= SecondWarningDistance)
     {
-        digitalWrite(greenPin, HIGH);
-        digitalWrite(yellowPins[0], HIGH);
-        digitalWrite(yellowPins[1], HIGH);
-        digitalWrite(redPin, LOW);
+        secondWarning();
     }
     else if (distanceCM <= FirstWarningDistance)
     {
-        digitalWrite(greenPin, HIGH);
-        digitalWrite(yellowPins[0], HIGH);
-        digitalWrite(yellowPins[1], LOW);
-        digitalWrite(redPin, LOW);
+        firstWarning();
     }
     else
     {
-        digitalWrite(greenPin, HIGH);
-        digitalWrite(yellowPins[0], LOW);
-        digitalWrite(yellowPins[1], LOW);
-        digitalWrite(redPin, LOW);
+        noWarning();
     }
 
     //LCD printing
@@ -80,11 +75,78 @@ void loop()
     lcd.print("Distance: ");
     lcd.print(distanceCM);
     lcd.print(" cm");
-    delay(dt);
+    delay(lcd_dt);
     lcd.clear();
 
     //Serial Monitor printing
     Serial.print("Distance: ");
     Serial.print(distanceCM);
     Serial.print(" cm\n");
+}
+
+void firstWarning()
+{
+    analogWrite(speakerPin, 0);
+    digitalWrite(greenPin, HIGH);
+    digitalWrite(yellowPins[0], HIGH);
+    digitalWrite(yellowPins[1], LOW);
+    digitalWrite(redPin, LOW);
+}
+
+void secondWarning()
+{
+    analogWrite(speakerPin, 0);
+    digitalWrite(greenPin, HIGH);
+    digitalWrite(yellowPins[0], HIGH);
+    digitalWrite(yellowPins[1], HIGH);
+    digitalWrite(redPin, LOW);
+}
+
+void thirdWarning(long distance, long thirdWarningValue)
+{
+    int dt = 100;
+    while(distance <= thirdWarningValue){
+        analogWrite(speakerPin, 100);
+        delay(dt);
+        analogWrite(speakerPin, 0);
+
+        break;
+    }
+    digitalWrite(greenPin, HIGH);
+    digitalWrite(yellowPins[0], HIGH);
+    digitalWrite(yellowPins[1], HIGH);
+    digitalWrite(redPin, LOW);
+}
+
+void finalWarning()
+{
+    analogWrite(speakerPin, 100);
+    digitalWrite(greenPin, HIGH);
+    digitalWrite(yellowPins[0], HIGH);
+    digitalWrite(yellowPins[1], HIGH);
+    digitalWrite(redPin, HIGH);
+}
+
+void noWarning()
+{
+    analogWrite(speakerPin, 0);
+    digitalWrite(greenPin, HIGH);
+    digitalWrite(yellowPins[0], LOW);
+    digitalWrite(yellowPins[1], LOW);
+    digitalWrite(redPin, LOW);
+}
+
+void redBlink(long distance, long maxValue)
+{
+
+    int dt = 50;
+    while (distance <= maxValue)
+    {
+        digitalWrite(redPin, HIGH);
+        delay(dt);
+        digitalWrite(redPin, LOW);
+        delay(dt);
+
+        break;
+    }
 }
