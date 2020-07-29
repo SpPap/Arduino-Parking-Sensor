3f4c4793c65aa60a4563111afa758081d23ff9c8
SpPap <spiros_pd@hotmail.com>
2020-07-29 17:55:51 +0300

    LCD monitoring

--- parking_sensor.ino
+++ parking_sensor.ino
@@ -1,28 +1,36 @@
+#include <LiquidCrystal.h>
+
 int echoPin = 2;
 int trigPin = 3;
 int redPin = 4;
 int greenPin = 5;
 int yellowPins[] = {6, 7};
+int rs = 12;
+int enable = 13;
+int d4 = 8, d5 = 9, d6 = 10, d7 = 11;
+
+LiquidCrystal lcd(rs, enable, d4, d5, d6, d7);
 
 void setup()
 {
+    Serial.begin(9600);
+    lcd.begin(16, 2);
 
     pinMode(trigPin, OUTPUT);
     pinMode(echoPin, INPUT);
 
     for (int i = 4; i <= 7; i++)
         pinMode(i, OUTPUT);
-
-    Serial.begin(9600);
 }
 
 void loop()
 {
 
     long duration = 0, distanceCM = 0;
-    long FirstWarningDistance = 100;
-    long SecondWarningDistance = 50 ;
-    long maxDistance = 20;
+    long FirstWarningDistance = 50;
+    long SecondWarningDistance = 25;
+    long maxDistance = 10;
+    int dt = 200; //delay time (LCD)
 
     //clear trigPin
     digitalWrite(trigPin, LOW);
@@ -67,6 +75,15 @@ void loop()
         digitalWrite(redPin, LOW);
     }
 
+    //LCD printing
+    lcd.setCursor(0, 0);
+    lcd.print("Distance: ");
+    lcd.print(distanceCM);
+    lcd.print(" cm");
+    delay(dt);
+    lcd.clear();
+
+    //Serial Monitor printing
     Serial.print("Distance: ");
     Serial.print(distanceCM);
     Serial.print(" cm\n");
